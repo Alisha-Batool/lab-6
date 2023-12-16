85b8995700d280d3e1ce4ea4204e3cb865ce8e4f
Alisha-Batool <144157470+Alisha-Batool@users.noreply.github.com>
2023-12-15 22:11:57 -0800

    write documentation

--- colorUtils.h
+++ colorUtils.h
@@ -30,13 +30,26 @@ int min(int a, int b, int c);
 
 /**
  * TODO: add documentation here
+ * 
  */
 
 int toGrayScale(int *r, int *g, int *b, Mode mode);
 
 /**
  * TODO: add documentation here
+ * @param r the red component of the color 
+ * @param g the green component of the color
+ * @param b the blue component of the color
+ * @param mode the mode to use for converting the color to grayscale
+ * @return the grayscale value of the color
  */
   
 
 int toSepia(int *r, int *g, int *b, Mode mode);
+/**
+ * @param r the red component of the color
+ * @param g the green component of the color 
+ * @param b the blue component of the color
+ * @param mode the mode to use for converting the color to sepia tone
+ * @return the sepia tone value of the color
+ * */
