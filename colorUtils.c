9be2b2a9d1915c987af1f14e8a5ada2d82448fe2
Alisha-Batool <144157470+Alisha-Batool@users.noreply.github.com>
2023-12-15 20:40:16 -0800

    write logic

--- colorUtils.c
+++ colorUtils.c
@@ -15,8 +15,13 @@ int toGrayScale(int *r, int *g, int *b, Mode mode) {
   //TODO: implement
   return ((*r + *g + *b) / 3);
   return ((max(*r, *g, *b) + min(*r, *g, *b)) / 2);
+  return (*r * 0.21 + *g * 0.72 + *b * 0.07);
+  
 }
 
 int toSepia(int *r, int *g, int *b) {
   //TODO: implement
+  return (*r * 0.393 + *g * 0.769 + *b * 0.189);
+  return (*r * 0.349 + *g * 0.686 + *b * 0.168);
+  return (*r * 0.272 + *g * 0.534 + *b * 0.131);
 }
